2c56e48f97ed11f7e53720b7a28330f5c41bab49
Helton Lopez <heltonjlopez@gmail.com>
2023-02-21 23:34:54 -0600

    Subiendo Tarea Parte II

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { interfaceResultado } from './interfaces/interfaceM';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  title = 'Resultados de partidos de Futbol';
+  descripcion = 'Esta es una aplicacion para leer ver marcadores de futbol'
+  
+  resultados: interfaceResultado[] = [
+    {
+      equipo1: 'Liverpool',
+      equipo2: 'Real Madrid',
+      marcadorE1: 2,
+      marcadorE2: 5,
+      descripcionJuego: 'Partido de ida de 8vos de la UCL',
+      fecha: '21 de Feb del 2023',
+      liga: 'UCL'
+    },
+
+    {
+      equipo1: 'Eintracht Frankfurt',
+      equipo2: 'Napoli',
+      marcadorE1: 0,
+      marcadorE2: 2,
+      descripcionJuego: 'Partido de ida de 8vos de la UCL',
+      fecha: '21 de Feb del 2023',
+      liga: 'UCL'
+    },
+
+    {
+      equipo1: 'PSG',
+      equipo2: 'LOSC',
+      marcadorE1: 4,
+      marcadorE2: 3,
+      descripcionJuego: 'Partido de liga, jornada 24',
+      fecha: '19 de Feb del 2023',
+      liga: 'Ligue 1'
+    },
+  ]
 }
---
+++ src/app/interfaces/interfaceM.ts
@@ -0,0 +1,9 @@
+  export interface interfaceResultado {
+    equipo1: string,
+    equipo2: string,
+    marcadorE1: number,
+    marcadorE2: number,
+    descripcionJuego: string,
+    fecha: string,
+    liga: string
+  }
